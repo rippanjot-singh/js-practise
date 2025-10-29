7cc916c1cd95c656ef1fa5e3ceaa2ba777f4f676
Rippanjot Singh <rippanjotsingh90@gmail.com>
2025-10-29 18:30:56 +0530

    practice-4-done

---
+++ 4-Character-Counter/script.js
@@ -0,0 +1,8 @@
+let inp = document.querySelector('input');
+let span = document.querySelector('span');
+
+inp.addEventListener("input", function (val){
+    span.textContent = inp.value.length
+    
+    // span.textContent = inp.
+});
